8815c355e3dbab08fc17981aabea9418662c08d1
Mohamed Sanaulla <sanaulla123@gmail.com>
2018-01-16 21:41:55 +0300

    API handlers for the REST APIs

--- 2_linksshr/src/main/java/com/packt/linksshr/handler/HttpRequestHandlers.java
+++ 2_linksshr/src/main/java/com/packt/linksshr/handler/HttpRequestHandlers.java
@@ -18,10 +18,13 @@ import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import com.mongodb.client.result.UpdateResult;
 import com.packt.linksshr.model.Comment;
 import com.packt.linksshr.model.Link;
+import com.packt.linksshr.model.User;
 import com.packt.linksshr.service.CommentService;
 import com.packt.linksshr.service.LinkService;
+import com.packt.linksshr.service.UserService;
 
 import ch.qos.logback.core.net.SyslogOutputStream;
 import reactor.core.publisher.Mono;
@@ -30,9 +33,9 @@ import reactor.core.publisher.Mono;
 public class HttpRequestHandlers {
 	@Autowired LinkService linkService;
 	@Autowired CommentService commentService;
+	@Autowired UserService userService;
 	
 	Mono<ServerResponse> message(ServerRequest request) {
-		System.out.println("Username is: " + request.principal().map(Principal::getName).block());
 		return request.principal()
 				.map(Principal::getName)
 				.flatMap(username ->
@@ -64,23 +67,68 @@ public class HttpRequestHandlers {
 		
 	}
 	
+	Mono<ServerResponse> getLink(ServerRequest request){
+		String linkId = request.pathVariable("linkId");
+		return ServerResponse.ok()
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(linkService.getLinkDetail(linkId), Link.class);
+	}
+	
 	Mono<ServerResponse> newComment(ServerRequest request){
 		return ServerResponse.ok()
 				.contentType(MediaType.APPLICATION_JSON)
-				.body(request.bodyToMono(Comment.class)
-						.doOnSuccess(c -> {
-							c.setPostedBy(request.principal().block().getName());
-							c.setPostedOn(LocalDateTime.now());
-							commentService.newComment(c);
-						}), Comment.class);
+				.body(request.principal().map(Principal::getName)
+						.flatMap( username -> {
+							return request.bodyToMono(Comment.class)
+									.flatMap(comment -> {
+										comment.setPostedBy(username);
+										comment.setPostedOn(LocalDateTime.now());
+										return commentService.newComment(comment);
+									});
+						}), UpdateResult.class);
+	}
+	
+	Mono<ServerResponse> getComments(ServerRequest request) {
+		String linkId = request.pathVariable("linkId");
+		return ServerResponse.ok()
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(commentService.getComments(linkId), Comment.class);
+	}
+	
+	Mono<ServerResponse> newUpVote(ServerRequest request){
+		String linkId = request.pathVariable("linkId");
+		return ServerResponse.ok()
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(linkService.addUpVote(linkId).flatMap(
+						result -> linkService.getLinkDetail(linkId)), Link.class);
+	}
+	
+	Mono<ServerResponse> newDownVote(ServerRequest request){
+		String linkId = request.pathVariable("linkId");
+		return ServerResponse.ok()
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(linkService.addDownVote(linkId).flatMap(
+						result -> linkService.getLinkDetail(linkId)), Link.class);
+	}
+	
+	Mono<ServerResponse> newUser(ServerRequest request){
+		return ServerResponse.ok()
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(request.bodyToMono(User.class).flatMap(
+						user -> userService.newUser(user)), User.class);
 	}
 	
 	@Bean
 	RouterFunction<?> routerFunctions() {
 		return RouterFunctions.route(RequestPredicates.GET("/message"), this::message)
 			.andRoute(RequestPredicates.GET("/api/links"), this::getLinks)
+			.andRoute(RequestPredicates.GET("/api/links/{linkId}"), this::getLink)
 			.andRoute(RequestPredicates.POST("/api/links"), this::newLink)
-			.andRoute(RequestPredicates.POST("/api/comments"), this::newComment)
+			.andRoute(RequestPredicates.POST("/api/links/{linkId}/upVote"), this::newUpVote)
+			.andRoute(RequestPredicates.POST("/api/links/{linkId}/downVote"), this::newDownVote)
+			.andRoute(RequestPredicates.POST("/api/links/{linkId}/comments"), this::newComment)
+			.andRoute(RequestPredicates.GET("/api/links/{linkId}/comments"), this::getComments)
+			.andRoute(RequestPredicates.POST("/api/users"), this::newUser)
 			;
 	}
 }
